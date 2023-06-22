e94e7bf8c3b8847118ece1c3697636aeba030276
tomasda7 <tomasda7@outlook.com>
2023-06-22 14:20:11 -0300

    Done exercise 4 of part3

--- index.js
+++ index.js
@@ -1,65 +1,71 @@
 const express = require('express');
 const app = express();
 
-const persons = [
-    {
-      "id": 1,
-      "name": "Arto Hellas",
-      "number": "040-123456"
-    },
-    {
-      "id": 2,
-      "name": "Ada Lovelace",
-      "number": "39-44-5323523"
-    },
-    {
-      "id": 3,
-      "name": "Dan Abramov",
-      "number": "12-43-234345"
-    },
-    {
-      "id": 4,
-      "name": "Mary Poppendieck",
-      "number": "39-23-6423122"
-    },
-    {
-      "id": 5,
-      "name": "Alan Turing",
-      "number": "4442231"
-    },
+let persons = [
+  {
+    "id": 1,
+    "name": "Arto Hellas",
+    "number": "040-123456"
+  },
+  {
+    "id": 2,
+    "name": "Ada Lovelace",
+    "number": "39-44-5323523"
+  },
+  {
+    "id": 3,
+    "name": "Dan Abramov",
+    "number": "12-43-234345"
+  },
+  {
+    "id": 4,
+    "name": "Mary Poppendieck",
+    "number": "39-23-6423122"
+  },
+  {
+    "id": 5,
+    "name": "Alan Turing",
+    "number": "4442231"
+  },
 ]
 
 app.get('/info', (request, response) => {
-    let date = new Date();
+  let date = new Date();
 
-    response.send(
-    `<h3>
-    the phone book has information about ${persons.length} persons <br/>
-    ${date}
-    <h3>`
-    );
+  response.send(
+  `<h3>
+  the phone book has information about ${persons.length} persons <br/>
+  ${date}
+  <h3>`
+  );
 });
 
 app.get('/api/persons', (request, response) => {
-    response.json(persons)
+  response.json(persons)
 });
 
 app.get('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id);
-    const person = persons.find(person => person.id === id);
+  const id = Number(request.params.id);
+  const person = persons.find(person => person.id === id);
 
-    if(person) {
-        response.json(person);
-    } else {
-        response.status(404).send('<h3>Person not found.<h3>')
-    }
+  if(person) {
+    response.json(person);
+  } else {
+    response.status(404).send('<h3>Person not found.<h3>')
+  }
 });
 
+app.delete('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id);
+  persons = persons.filter(person => person.id !== id);
+
+  response.sendStatus(204);
+})
 
 
 
 const PORT = 3001;
 
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 })
